b9df4c7c134216454cf9993b2bb18ebfb0326c9b
Vinay Pratap <vinaypratapknj12@gmail.com>
2024-08-25 14:27:19 +0530

    add cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+/// cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+/// uploadOnCloudinary funtion
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        const uploadResult = await cloudinary.uploader
+            .upload(
+                localFilePath, {
+                resource_type: "auto"
+            }
+            )
+        console.log(`file uploade on cloudinary: ${uploadResult}`)
+        console.log(`file uploade on cloudinary: ${uploadResult.url}`)
+        fs.unlinkSync(localFilePath)
+        return uploadResult
+
+
+    }
+    catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
